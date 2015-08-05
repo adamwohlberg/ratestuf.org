073887b13aed9bb1db002e84c486970edc810e07
adamzuckerberg <adam@adamzuckerberg.com>
2015-08-05 14:17:39 -0700

    Update config file

--- inc/config.php
+++ inc/config.php
@@ -19,7 +19,7 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
   define("DB_SERVER", $url["host"]);
   define("DB_USER", $url["user"]);
   define("DB_PASS", $url["pass"]);
-  define("DB_NAME",substr($url["path"], 1);
+  define("DB_NAME",substr($url["path"], 1));
   
 
   // update a password: 
